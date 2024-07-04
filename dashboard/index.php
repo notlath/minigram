121a6a6835215ae1f4f6afa3851649ca99b87e2e
Lathrell Pagsuguiron <qltpagsuguiron@tip.edu.ph>
2024-07-05 07:28:30 +0800

    pangatlo na to tangina

--- dashboard/index.php
+++ dashboard/index.php
@@ -4,19 +4,6 @@ include "header.php";
 $sql_posts = "SELECT * FROM posts ORDER BY timestamp DESC";
 $res_posts = mysqli_query($connect, $sql_posts);
 
-echo "
-<center>
-<div class='table-responsive' style='width: 90%'>
-
-    <table class='table table-bordered'>
-        <tr>
-            <th width='7%'>POST ID</th>
-            <th width='40%'>IMAGE</th>
-            <th width='10%'>DATE POSTED</th>
-            <th width='25%'>DESCRIPTION</th>
-        </tr>
-";
-
 while($row_posts = mysqli_fetch_array($res_posts))
 {
     $postid = $row_posts['postid'];
@@ -24,34 +11,27 @@ while($row_posts = mysqli_fetch_array($res_posts))
     $timestamp = $row_posts['timestamp'];
     $text = $row_posts['text'];
 
-    echo '<tr>';
-
-        echo "<td>";
-            echo "<center>";
-                echo $postid;
-            echo "</center>";
-        echo "</td>";
-
-        echo "<td>";
-            echo "<img src='../uploads/".$image."' alt='".$image."'/>";
-        echo "</td>";
-        
-        echo "<td>";
-            echo date('Y-m-d', strtotime($timestamp)); // Format the timestamp to only show the date
-        echo "</td>";
-        
-        echo "<td>";
-            echo $text;
-        echo "</td>";
-
-    echo '</tr>';
+    echo "<center><div class='table-responsive' style='width: 90%'>";
+    echo "<table class='table table-bordered'>";
+    
+    // POST ID
+    echo "<tr><th width='20%'>POST ID</th>";
+    echo "<td>".$postid."</td></tr>";
+    
+    // IMAGE
+    echo "<tr><th>IMAGE</th>";
+    echo "<td><img src='../uploads/".$image."' alt='".$image."'/></td></tr>";
+    
+    // DATE POSTED
+    echo "<tr><th>DATE POSTED</th>";
+    echo "<td>".date('Y-m-d', strtotime($timestamp))."</td></tr>";
+    
+    // TEXT
+    echo "<tr><th>TEXT</th>";
+    echo "<td>".$text."</td></tr>";
+    
+    echo "</table></div></center>";
 }
-
-
-echo " 
-    </table>
-    </div>
-    </center>";
 
 ?>
 
--- dashboard/my_posts.php
+++ dashboard/my_posts.php
@@ -4,38 +4,29 @@ $userid = $_SESSION['userid'];
 $sql_posts = "SELECT * FROM posts WHERE userid='".$userid."'";
 $res_posts = mysqli_query($connect, $sql_posts);
 
-echo "
-<center>
-<div class='table-responsive' style='width: 90%'>
-    <table class='table table-bordered'>
-        <tr>
-            <th width='7%'>POST ID</th>
-            <th width='40%'>IMAGE</th>
-            <th width='10%'>DATE POSTED</th>
-            <th width='25%'>TEXT</th>
-        </tr>
-";
 while($row_posts = mysqli_fetch_array($res_posts))
 {
-    echo '<tr>';
-        echo "<td><center>";
-            echo $row_posts['postid'];
-        echo "</center></td>";
-        echo "<td>";
-            echo "<img src='../uploads/".$row_posts['image']."' alt='".$row_posts['image']."'/>";
-        echo "</td>";
-        echo "<td>";
-            echo date('Y-m-d', strtotime($row_posts['timestamp']));
-        echo "</td>";
-        echo "<td>";
-            echo $row_posts['text'];
-        echo "</td>";
-    echo '</tr>';
+    echo "<center><div class='table-responsive' style='width: 90%'>";
+    echo "<table class='table table-bordered'>";
+    
+    // POST ID
+    echo "<tr><th width='20%'>POST ID</th>";
+    echo "<td>".$row_posts['postid']."</td></tr>";
+    
+    // IMAGE
+    echo "<tr><th>IMAGE</th>";
+    echo "<td><img src='../uploads/".$row_posts['image']."' alt='".$row_posts['image']."'/></td></tr>";
+    
+    // DATE POSTED
+    echo "<tr><th>DATE POSTED</th>";
+    echo "<td>".date('Y-m-d', strtotime($row_posts['timestamp']))."</td></tr>";
+    
+    // TEXT
+    echo "<tr><th>TEXT</th>";
+    echo "<td>".$row_posts['text']."</td></tr>";
+    
+    echo "</table></div></center>";
 }
-echo " 
-</table>
-</div>
-</center>";
 ?>
 
 <script>
